bfe3d42464de18261f37a9181e1411803f560272
Briun Greene <harmonickarma789@gmail.com>
2026-04-22 16:26:31 -0400

    Extended testing criteria

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -7,6 +7,8 @@ namespace UnitTestingExercise.Tests
     {
         [Theory]
         [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(2, 13, 5, 20)]
+        [InlineData(20, 3, 7, 30)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -26,6 +28,8 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(4, 3, 1)]//Add test data <-------
+        [InlineData(5, 3, 2)]
+        [InlineData(6, 3, 3)]
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -41,6 +45,8 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(5,6, 30)]//Add test data <-------
+        [InlineData(6, 3, 18)]
+        [InlineData(9, 3, 27)]
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -57,6 +63,8 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(8, 2, 4)]//Add test data <-------
+        [InlineData(6, 3, 2)]
+        [InlineData(18, 3, 6)]
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
